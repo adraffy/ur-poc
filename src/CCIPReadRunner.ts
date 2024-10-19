10e8b9c0f55444627a1583c35a2ef1977b86685e
Andrew Raffensperger <raffy@me.com>
2024-10-18 18:37:07 -0700

    fix unanswered

--- src/CCIPReadRunner.ts
+++ src/CCIPReadRunner.ts
@@ -28,6 +28,10 @@ type OffchainLookup = OffchainSender & {
 	carry: string;
 };
 
+function isLookup(x: OffchainLookup | OffchainSender): x is OffchainLookup {
+	return "urls" in x;
+}
+
 export class CCIPReadRunner implements ContractRunner {
 	constructor(readonly runner: ContractRunner, readonly maxAttempts = 20) {}
 	get provider() {
@@ -38,24 +42,23 @@ export class CCIPReadRunner implements ContractRunner {
 		if (!tx0.to || !tx0.enableCcipRead) return this.runner.call!(tx0);
 		// force resolve the target and remember it
 		const origin = await resolveAddress(tx0.to);
-		let lookup = await this._call(
-			{
-				...tx0, // call the original tx
-				to: origin,
-				enableCcipRead: false, // disable ccip-read
-			},
-			false // disable OffchainTryNext()
-		);
+		let lookup = await this._call({
+			...tx0, // call the original tx
+			to: origin,
+			enableCcipRead: false, // disable ccip-read
+		});
 		if (typeof lookup === "string") return lookup; // it answered immediately
 		// [ERC-3668] sender must match
 		if (lookup.sender !== origin) throw new Error("origin != sender");
+		if (!isLookup(lookup)) throw new Error("unexpected next()");
 		let index = 0;
+		let prev = lookup;
 		for (let n = this.maxAttempts; n > 0; n--) {
 			let url: string;
 			let response: string;
-			if (index < lookup.urls.length) {
+			if (index < prev.urls.length) {
 				// we still have endpoints to try
-				url = lookup.urls[index++];
+				url = prev.urls[index++];
 				try {
 					// [ERC-3668] build request according
 					const options: RequestInit = {};
@@ -63,16 +66,17 @@ export class CCIPReadRunner implements ContractRunner {
 						options.method = "POST";
 						options.body = JSON.stringify({
 							sender: origin,
-							data: lookup.request,
+							data: prev.request,
 						});
 					}
-					url = url.replaceAll("{data}", lookup.request);
+					url = url.replaceAll("{data}", prev.request);
 					url = url.replaceAll("{sender}", origin);
 					const res = await fetch(url, options); // call it
 					// ignore res.status, assume json
 					const { data } = await res.json();
 					// [ERC-3668] extract {data}, ensure hex string
 					response = hexlify(data);
+					if (response == UNANSWERED) continue; // not allowed
 				} catch (err) {
 					continue;
 				}
@@ -82,37 +86,30 @@ export class CCIPReadRunner implements ContractRunner {
 			}
 			// [ERC-3668] build callback response
 			const data = concat([
-				lookup.callback,
+				prev.callback,
 				AbiCoder.defaultAbiCoder().encode(
 					["bytes", "bytes"],
-					[response, lookup.carry]
+					[response, prev.carry]
 				),
 			]);
-			const next = await this._call({ to: origin, data }, true);
+			const next = await this._call({ to: origin, data });
 			if (typeof next === "string") return next; // answered immediately
+			if (response === UNANSWERED && !isLookup(next))
+				throw new Error("unexpected next()");
 			// [ERC-3668] recursive, sender must still match
 			if (next.sender !== origin) throw new Error("origin != sender");
 			// if next is OffchainTryNext(), just continue
 			// if next is a new OffchainLookup(), reset iterator and keep going
-			if ("urls" in next) {
+			if (isLookup(next)) {
 				index = 0;
-				lookup = next;
+				prev = next;
 			}
 		}
 		throw new Error(`'ccip read: max attempts (${this.maxAttempts})`);
 	}
 
 	async _call(
-		tx: TransactionRequest,
-		canNext: false
-	): Promise<string | OffchainLookup>;
-	async _call(
-		tx: TransactionRequest,
-		canNext: true
-	): Promise<string | OffchainLookup | OffchainSender>;
-	async _call(
-		tx: TransactionRequest,
-		canNext: boolean
+		tx: TransactionRequest
 	): Promise<string | OffchainLookup | OffchainSender> {
 		try {
 			return await this.runner.call!(tx);
@@ -123,7 +120,7 @@ export class CCIPReadRunner implements ContractRunner {
 			const error = ABI.parseError(err.data);
 			if (error?.name === "OffchainLookup") {
 				return <OffchainLookup>error.args.toObject();
-			} else if (canNext && error?.name === "OffchainTryNext") {
+			} else if (error?.name === "OffchainTryNext") {
 				return <OffchainSender>error.args.toObject();
 			}
 			throw err;
